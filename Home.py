dfb11b292b295b46af20eb608da3f24c590c7f7b
Mayur Balu Dabade <mayurdabade1103@gmail.com>
2024-03-17 17:51:05 +0530

    update

--- Home.py
+++ Home.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.title("SynthSite: Summarize & Chat Web Platform")
+st.title("✨🚀SynthSite: Summarize & Chat Web Platform ")
+
+st.write("SynthSite is a dynamic web platform that offers two key features: summarizing web content and enabling chat functionality. Users can input a website URL, and SynthSite will generate a concise summary of the content. Additionally, users can engage in real-time chat discussions related to the website's topic. It's a versatile tool for both consuming information efficiently and fostering interactive discussions. 🌐")
+
+st.write("~ Mayur Dabade")
